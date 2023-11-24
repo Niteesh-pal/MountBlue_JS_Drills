26d98b32f2428bd62a7146a07839602a7adf24fa
Niteesh <niteeshpal801@gmail.com>
2023-11-24 15:09:09 +0530

    done with q1 and q2

---
+++ callback/fs-problem1.cjs
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+let randomNumberOfFiles = (Math.random() + 1) * 5;
+randomNumberOfFiles = Math.floor(randomNumberOfFiles);
+
+const absolutePath = './callback/output';
+
+function fsProblem1(path, randomFiles) {
+  fs.access(path + '/fsProblem1', (err) => {
+    if (err) {
+      fs.mkdir(path + '/fsProblem1', (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log();
+          createRandomFiles(path + '/fsProblem1/', randomFiles);
+        }
+      });
+    } else {
+      createRandomFiles(path + '/fsProblem1/', randomFiles);
+    }
+  });
+}
+
+function createRandomFiles(path, randomFiles) {
+  for (let i = 1; i <= randomFiles; i++) {
+    fs.writeFile(path + `file${i}.txt`, '', (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`file created file${i}.txt`);
+        deleteRandomFiles(path + `file${i}.txt`);
+      }
+    });
+  }
+}
+
+function deleteRandomFiles(path) {
+  fs.unlink(path, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`file deleted `);
+    }
+  });
+}
+
+fsProblem1(absolutePath, randomNumberOfFiles);
---
+++ callback/fs-problem2.cjs
@@ -0,0 +1,145 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = './lipsum.txt';
+
+function fsProblem2(filePath, convertUpperCase, convertLowerCase) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      convertUpperCase(data, storeNameOfFile, convertLowerCase);
+      // convertLowerCase(readFileName, storeNameOfFile);
+    }
+  });
+}
+
+function readFileName(data, callback) {
+  fs.readFile(data, 'utf-8', (err, file) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let fileList = file.split('\n');
+      // console.log(fileList);
+      callback(fileList[fileList.length - 2].trim());
+    }
+  });
+}
+function writeNewFile(path, filename, sentences) {
+  fs.writeFile(path + '/' + filename, sentences, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`file Created Successfull ${filename}`);
+    }
+  });
+}
+function storeNameOfFile(data) {
+  fs.appendFile('./filenames.txt', `${data}\n`, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('file name stored');
+    }
+  });
+}
+
+function convertLowerCase(
+  readFileName,
+  storeNameOfFile,
+  writeNewFile,
+  sortContentofNewFile
+) {
+  readFileName('./filenames.txt', (fileName) => {
+    fs.readFile(`./output/fsProblem2/${fileName}`, 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let content = data.toLocaleLowerCase();
+        let sentences = content.split('.');
+        writeNewFile(
+          './output/fsProblem2',
+          'covertToLowerCaseSentence.txt',
+          sentences.join('\n')
+        );
+
+        storeNameOfFile('covertToLowerCaseSentence.txt');
+
+        sortContentofNewFile(
+          readFileName,
+          storeNameOfFile,
+          writeNewFile,
+          deleteFile
+        );
+      }
+    });
+  });
+}
+
+function sortContentofNewFile(
+  readFileName,
+  storeNameOfFile,
+  writeNewFile,
+  deleteFile
+) {
+  readFileName('./filenames.txt', (fileName) => {
+    fs.readFile('./output/fsProblem2/' + fileName, 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // let content = data.split('\n');
+        // console.log(content);
+        // console.log(data);
+        let content = data.split('\n').sort();
+        writeNewFile(
+          './output/fsProblem2',
+          'sortContentToFile.txt',
+          content.toString()
+        );
+
+        storeNameOfFile('sortContentToFile.txt');
+        deleteFile();
+      }
+    });
+  });
+}
+
+function convertUpperCase(data, storeNameOfFile, convertLowerCase) {
+  fs.writeFile(
+    './output/fsProblem2/convertToUpperCase.txt',
+    data.toUpperCase(),
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        storeNameOfFile('convertToUpperCase.txt');
+        convertLowerCase(
+          readFileName,
+          storeNameOfFile,
+          writeNewFile,
+          sortContentofNewFile
+        );
+      }
+    }
+  );
+}
+
+function deleteFile() {
+  fs.readFile('./filenames.txt', 'utf-8', (err, fileList) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let list = fileList.split('\n');
+      for (let i = 0; i < list.length - 1; i++) {
+        fs.unlink(`./output/fsProblem2/${list[i]}`, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('succesfully deleted');
+          }
+        });
+      }
+    }
+  });
+}
+
+fsProblem2(filePath, convertUpperCase, convertLowerCase);
